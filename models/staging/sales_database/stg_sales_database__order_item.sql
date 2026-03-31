e234c41095fd59d38f8b4d120e2f1d8ce490c000
Cedric <c.lecat.cl@gmail.com>
2026-03-31 15:20:01 +0200

    :q!

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,10 +1,11 @@
-select CONCAT(order_id, '_', product_id) as order_item_id_y,
- order_id as order_id_y,
- product_id as product_id_y,
- seller_id as seller_id_y,
+<<<<<<< HEAD
+select 
  DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_y,
- price as unit_price_y,
- shipping_cost_y,
- quantity as item_quantity_y,
- (price * quantity) + shipping_cost as total_order_item_amount_y
+ order_id AS order_id_x,
+ product_id AS product_id_x,
+ seller_id AS seller_id_x,
+ price as unit_price_x,
+ shipping_cost_x,
+ quantity as item_quantity_x,
+ (price * quantity) + shipping_cost as total_order_item_amount_x
 from {{ source('sales_database', 'order_item') }}
